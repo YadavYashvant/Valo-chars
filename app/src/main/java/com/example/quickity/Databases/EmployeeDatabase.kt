dfd0a10bf5760eeaf61a13ae9af8fae69fed7b3b
yashvant <yashvantyadav855@gmail.com>
2023-11-21 16:40:06 +0530

    making Database to implement roomDB

---
+++ app/src/main/java/com/example/quickity/Databases/EmployeeDatabase.kt
@@ -0,0 +1,40 @@
+package com.example.quickity.Databases
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.quickity.Daos.EmployeeDao
+import com.example.quickity.models.Employee
+
+@Database(entities = [(Employee::class)], version = 1, exportSchema = false)
+abstract class EmployeeDatabase : RoomDatabase() {
+
+    abstract fun employeeDao(): EmployeeDao
+
+    companion object{
+        @Volatile
+        private var INSTANCE: EmployeeDatabase? = null
+
+        fun getInstance(context: Context): EmployeeDatabase {
+            // only one thread of execution at a time can enter this block of code
+            synchronized(this) {
+                var instance = INSTANCE
+
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        EmployeeDatabase::class.java,
+                        "employee_database"
+                    ).fallbackToDestructiveMigration()
+                        .build()
+
+                    INSTANCE = instance
+                }
+                return instance
+            }
+        }
+    }
+
+
+}
