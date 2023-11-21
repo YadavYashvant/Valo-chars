55d2f022d3b648a33725a3508cfc6b10f72523a6
yashvant <yashvantyadav855@gmail.com>
2023-11-21 16:44:56 +0530

    making Repository to implement roomDB

---
+++ app/src/main/java/com/example/quickity/Repositories/EmployeeRepository.kt
@@ -0,0 +1,29 @@
+package com.example.quickity.Repositories
+
+import androidx.lifecycle.MutableLiveData
+import com.example.quickity.Daos.EmployeeDao
+import com.example.quickity.models.Employee
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class EmployeeRepository(private val employeeDao: EmployeeDao) {
+
+    val allEmployees = MutableLiveData<List<Employee>>()
+    val foundEmployee = MutableLiveData<Employee>()
+    private val coroutineScope = CoroutineScope(Dispatchers.Main)
+
+    fun addEmployee(newEmployee: Employee) {
+        coroutineScope.launch(Dispatchers.IO) {
+            employeeDao.addEmployee(newEmployee)
+        }
+    }
+
+    fun updateEmployeeDetails(newEmployee: Employee) {
+        coroutineScope.launch(Dispatchers.IO) {
+            employeeDao.updateEmployeeDetails(newEmployee)
+        }
+    }
+
+
+}
