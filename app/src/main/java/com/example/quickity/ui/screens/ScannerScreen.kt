d4baec3190725a7d3d03eadd835b9bb036f68dcd
yashvant <yashvantyadav855@gmail.com>
2023-12-06 12:19:37 +0530

    adding qr code analyzer feature

--- app/src/main/java/com/example/quickity/ui/screens/ScannerScreen.kt
+++ app/src/main/java/com/example/quickity/ui/screens/ScannerScreen.kt
@@ -2,6 +2,7 @@ package com.example.quickity.ui.screens
 
 import android.content.pm.PackageManager
 import android.util.Size
+import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.camera.core.CameraSelector
