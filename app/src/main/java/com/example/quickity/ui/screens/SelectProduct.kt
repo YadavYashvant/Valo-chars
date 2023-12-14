cb74c489bce119884e54e46b2bb47fec0c542413
yashvant <yashvantyadav855@gmail.com>
2023-12-14 22:21:32 +0530

    making new screen

--- app/src/main/java/com/example/quickity/ui/screens/SelectProduct.kt
+++ app/src/main/java/com/example/quickity/ui/screens/SelectProduct.kt
@@ -1,2 +1,10 @@
 package com.example.quickity.ui.screens
 
+import androidx.compose.runtime.Composable
+
+// This contains UI screen for selecting a product
+
+@Composable
+fun SelectProduct() {
+
+}
