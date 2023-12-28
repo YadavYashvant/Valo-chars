af18288641983ede5e3dd4596932db9fe937e979
yadavyashvant <yashvantyadav855@gmail.com>
2023-12-28 13:43:17 +0530

    adding audio

--- app/src/main/java/com/example/quickity/api_feature/ui/screen/detail/DetailAgent.kt
+++ app/src/main/java/com/example/quickity/api_feature/ui/screen/detail/DetailAgent.kt
@@ -2,15 +2,12 @@ package com.example.quickity.api_feature.ui.screen.detail
 
 import android.annotation.SuppressLint
 import android.media.MediaPlayer
-import android.net.Uri
-import android.widget.Space
 import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.spring
-import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.ExperimentalFoundationApi
@@ -24,30 +21,28 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.VerticalPager
 import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.Call
 import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.Pause
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -55,6 +50,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.paint
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
@@ -65,12 +61,12 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import coil.compose.rememberAsyncImagePainter
+import com.example.quickity.R
 import com.example.quickity.api_feature.apidata.DataItem
 import com.example.quickity.api_feature.viewmodel.HomeViewModel
 import com.example.quickity.ui.theme.blackV
 import com.example.quickity.ui.theme.redV
 import com.example.quickity.ui.theme.tungstenFont
-import okhttp3.internal.wait
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
@@ -84,6 +80,8 @@ fun AgentsHeader(
     val mContext = LocalContext.current
     //val audioattr = Uri.parse(data?.voiceLine?.medialist?.get(0)?.wave)
     //val mMediaPlayer = MediaPlayer.create(mContext, audioattr)
+    val mediaPlayer = MediaPlayer.create(mContext, R.raw.demogym)
+    var isPlaying by remember{ mutableStateOf(false) }
 
     when (pagerIndex) {
         0 -> {
@@ -234,35 +232,54 @@ fun AgentsHeader(
                         ),
                     )
                     Box() {
-
                         Image(
                             painter = rememberAsyncImagePainter(model = data?.displayIcon),
                             contentDescription = "Display Icon",
                             modifier = Modifier
                                 .size(90.dp)
                                 .align(Alignment.Center)
+                                .clip(RoundedCornerShape(20.dp))
                         )
                         IconButton(
-                            onClick = { /*mMediaPlayer.start()*/ }, modifier = Modifier.align(
+                            onClick = {
+                                      /*mMediaPlayer.start()*/
+
+                                      if(isPlaying) {
+                                          mediaPlayer.release()
+                                      }else{
+                                          mediaPlayer.start()
+                                      }
+                                isPlaying = !isPlaying
+
+                                      }, modifier = Modifier.align(
                                 Alignment.Center
                             )
                         ) {
                             Icon(
-                                imageVector = Icons.Default.PlayArrow,
+                                imageVector = if(isPlaying) {
+                                                            Icons.Default.Pause
+                                                            }else{
+                                                                 Icons.Default.PlayArrow
+                                                                 },
                                 contentDescription = "",
                                 tint = Color.White,
                                 modifier = Modifier.fillMaxSize(1f)
                             )
                         }
                     }
+
                 }
-
-
             }
         }
+
     }
 
-
+    DisposableEffect(Unit) {
+        onDispose {
+            mediaPlayer.release()
+            isPlaying = false
+        }
+    }
 }
 
 
